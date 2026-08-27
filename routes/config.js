0d3e4f6cd2f2049818f859bf30e5676e332aabbd
ThamonwanS. <75828311+thamonwan-s@users.noreply.github.com>
2026-08-28 04:21:22 +0700

    Update config.js

--- routes/config.js
+++ routes/config.js
@@ -1,4 +1,4 @@
 const LOGO_CONFIG = {
     BMTA: "https://upload.wikimedia.org/wikipedia/commons/5/58/BMTA_Logo2014-th.svg?utm_source=th.wikipedia.org&utm_campaign=index&utm_content=original",
-    TSB: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Thai_Smile_Group_Logo.svg/1280px-Thai_Smile_Group_Logo.svg.png?utm_source=th.wikipedia.org&utm_campaign=index&utm_content=thumbnail",
+    TSB: "https://thaismilegroup.com/wp-content/uploads/2024/05/logoweb-02_0-500x500.png",
 };
